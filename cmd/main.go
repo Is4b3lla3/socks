f72673e8bf1e9ce0fbcca31114acc9d52a6c056f
Is4b3lla3 <136045033@qq.com>
2022-06-09 17:08:22 +0800

    add cli prase

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"flag"
 	"github.com/is4b3lla3/socks"
 	"log"
 )
 
 func main() {
+	ip := flag.String("ip", "127.0.0.1", "请输入监听地址")
+	port := flag.Int("port", 1090, "请输入端口号")
+	flag.Parse()
+
 	server := socks.SocksServer{
-		IP:   "localhost",
-		Port: 1091,
+		IP:   *ip,
+		Port: *port,
 	}
+	log.Println("Usage: ./socks -ip=192.168.3.1 -port=1080")
+	log.Printf("Socks5Server is runs on %s:%d", server.IP, server.Port)
 	err := server.Run()
 	if err != nil {
 		log.Fatal(err)
